8e14407d623ec06d6313a011ea8a388e54c083f9
Denny Ly <denny_97_ly@hotmail.com>
2016-04-06 09:17:12 +0200

    first commit!

---
+++ commandpromptspel_denny_patrik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commandpromptspel_denny_patrik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
