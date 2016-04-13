b27759c43728705d4d9604bb2b59f41c1dfb2d47
Denny Ly <denny_97_ly@hotmail.com>
2016-04-13 11:09:40 +0200

    Banan träd

--- commandpromptspel_denny_patrik/Program.cs
+++ commandpromptspel_denny_patrik/Program.cs
@@ -4,14 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace commandpromptspel_denny_patrik
+namespace karl_axels_cmd_adventure
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("panda lol gitgud");
+            Boolean harYxa = false;
+            Boolean träd = false;
+            string input;
+
+            Console.WriteLine("---Välkommen till Panda försvunna äventyr---");
+            Console.WriteLine("\nDu vaknar upp i en jungle. Det enda som finns är träd och ett temple bakom träden. Det ligger en yxa på marken");
+
+            while (true) //oändlig loop
+            {
+                Console.Write("\n>");
+                input = Console.ReadLine();
+
+                if (input == "träd" || input == "gå fram" || input == "gå fram till träd")
+                {
+                    if (harYxa)
+                    {
+                        Console.WriteLine("hugga ner träden");
+                        träd = true;
+                    }
+                    else
+                        Console.WriteLine("Träden i vägen");
+                }
+                //test//
+               else if (input == "gå fram" || input == "gå" || input == "gå fram till tempel")
+                {
+                
+                    if (träd)
+                    {
+                        Console.WriteLine("du gick fram till tempeln");        
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Träden i vägen");
+                }
+                //test//
+                else if (input == "plocka upp yxa" || input == "ta yxan")
+                {
+                    Console.WriteLine("Du tog upp yxan använd den för att hugga ner träden");
+                    harYxa = true;
+                }
+                else 
+                    Console.WriteLine("Icke gilltigt eller felstavat commando!");
+
+            }
+            
+        //    Console.WriteLine("\nDu vann!!! Spelet stängs av...");
             Console.ReadLine();
         }
     }
 }
+
+
