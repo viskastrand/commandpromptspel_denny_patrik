ac76da71475148dcb9d4f64bf913676dece2443b
Patrik Stenström <coolmanutd_98@hotmail.com>
2016-04-20 11:10:18 +0200

    Big Business Bro

--- commandpromptspel_denny_patrik/Program.cs
+++ commandpromptspel_denny_patrik/Program.cs
@@ -8,59 +8,87 @@ namespace karl_axels_cmd_adventure
 {
     class Program
     {
+        static Boolean gameover;
+        static Boolean gamecomplete;
+        static Boolean harYxa;
+        static Boolean träd;
+        static string input;
         static void Main(string[] args)
         {
-            Boolean harYxa = false;
-            Boolean träd = false;
-            string input;
-
-            Console.WriteLine("---Välkommen till Panda försvunna äventyr---");
-            Console.WriteLine("\nDu vaknar upp i en jungle. Det enda som finns är träd och ett temple bakom träden. Det ligger en yxa på marken");
-
-            while (true) //oändlig loop
+            while (true)
             {
-                Console.Write("\n>");
+                gamecomplete = false;
+                gameover = false;
+                harYxa = false;
+
+                Console.WriteLine("---Välkommen till Panda försvunna äventyr---");
+                Console.WriteLine("\nDu vaknar upp i en jungle. Det enda som finns är träd och ett temple bakom träden. Det ligger en yxa på marken");
+                skog();
+                if (gameover)
+                    Console.WriteLine("\nDu förlorade!!! Vill du spela igen? y/n");
+                else
+                    Console.WriteLine("\nDu vann!!! Vill du spela igen? y/n");
                 input = Console.ReadLine();
-
-                if (input == "träd" || input == "gå fram" || input == "gå fram till träd")
+                if (input == "n" || input == "no" || input == "nej")
                 {
-                    if (harYxa)
-                    {
-                        Console.WriteLine("hugga ner träden");
-                        träd = true;
-                    }
-                    else
-                        Console.WriteLine("Träden i vägen");
+                    Console.WriteLine("Spelet stängs av...");
+                    break;
                 }
-                //test//
-               else if (input == "gå fram" || input == "gå" || input == "gå fram till tempel")
-                {
-                
-                    if (träd)
-                    {
-                        Console.WriteLine("du gick fram till tempeln");        
-                        break;
-                    }
-                    else
-                        Console.WriteLine("Träden i vägen");
-                }
-                //test//
-                else if (input == "plocka upp yxa" || input == "ta yxan")
-                {
-                    Console.WriteLine("Du tog upp yxan använd den för att hugga ner träden");
-                    harYxa = true;
-                }
-                else 
-                    Console.WriteLine("Icke gilltigt eller felstavat commando!");
-
+                Console.WriteLine("Spelet startas om...");
+                Console.ReadLine();
             }
-            
-        //    Console.WriteLine("\nDu vann!!! Spelet stängs av...");
-            Console.ReadLine();
+        }
 
-         //testtest
+
+            private static void skog() {
+            while (true) {
+                    Console.Write("\n>");
+                    input = Console.ReadLine();
+
+                    if (input == "träd" || input == "gå fram" || input == "gå fram till träd")
+                    {
+                        if (harYxa)
+                        {
+                            Console.WriteLine("hugga ner träden");
+                            träd = true;
+                        }
+                        else
+                            Console.WriteLine("Träden i vägen");
+                    }
+                    //test//
+                    else if (input == "gå fram" || input == "gå" || input == "gå fram till tempel")
+                    {
+
+                        if (träd)
+                        {
+                            Console.WriteLine("du gick fram till tempeln");
+                            break;
+                        }
+                        else
+                            Console.WriteLine("Träden i vägen");
+                    }
+                    //test//
+                    else if (input == "plocka upp yxa" || input == "ta yxan")
+                    {
+                        Console.WriteLine("Du tog upp yxan använd den för att hugga ner träden");
+                        harYxa = true;
+                    }
+                    else
+                        Console.WriteLine("Icke gilltigt eller felstavat commando!");
+
+                
+            }
+            tempel();
+    }
+
+        private static void tempel()
+        {
+            while (true)
+            {
+                
+            }
         }
     }
 }
-
+    
 
