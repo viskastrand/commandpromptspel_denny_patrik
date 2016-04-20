2a5d3d75f960e8acbd6c329b7b24d9380bb7dba1
Patrik Stenström <coolmanutd_98@hotmail.com>
2016-04-20 11:39:34 +0200

    3.1

--- commandpromptspel_denny_patrik/Program.cs
+++ commandpromptspel_denny_patrik/Program.cs
@@ -12,21 +12,42 @@ namespace karl_axels_cmd_adventure
         static Boolean gamecomplete;
         static Boolean harYxa;
         static Boolean träd;
+        static Boolean ljus;
         static string input;
         static void Main(string[] args)
         {
-            Boolean harYxa = false;
-            Boolean träd = false;
-            string input;
+            while (true)
+            {
+                gamecomplete = false;
+                gameover = false;
+                harYxa = false;
+
+                Console.WriteLine("---Välkommen till Panda försvunna äventyr---");
+                Console.WriteLine("\nDu vaknar upp i en jungle. Det enda som finns är träd och ett temple bakom träden. Det ligger en yxa på marken");
+                skog();
+                if (gameover)
+                    Console.WriteLine("\nDu förlorade!!! Vill du spela igen? y/n");
+                else
+                    Console.WriteLine("\nDu vann!!! Vill du spela igen? y/n");
+                input = Console.ReadLine();
+                if (input == "n" || input == "no" || input == "nej")
+                {
+                    Console.WriteLine("Spelet stängs av...");
+                    break;
+                }
+                Console.WriteLine("Spelet startas om...");
+                Console.ReadLine();
+            }
+        }
 
 
-            Console.WriteLine("---Välkommen till Panda försvunna äventyr---");
-            Console.WriteLine("\nDu vaknar upp i en jungle. Det enda som finns är träd och ett temple bakom träden. Det ligger en yxa på marken");
-
-            while (true) //oändlig loop
+        private static void skog()
+        {
+            while (true)
             {
                 Console.Write("\n>");
                 input = Console.ReadLine();
+
                 if (input == "träd" || input == "gå fram" || input == "gå fram till träd")
                 {
                     if (harYxa)
@@ -35,18 +56,15 @@ namespace karl_axels_cmd_adventure
                         träd = true;
                     }
                     else
-                    {
-                        
-                    }
                         Console.WriteLine("Träden i vägen");
                 }
                 //test//
-               else if (input == "gå fram" || input == "gå" || input == "gå fram till tempel")
+                else if (input == "gå fram" || input == "gå" || input == "gå fram till tempel")
                 {
-                
+
                     if (träd)
                     {
-                        Console.WriteLine("du gick fram till tempeln");        
+                        Console.WriteLine("du gick fram till tempeln");
                         break;
                     }
                     else
@@ -58,19 +76,40 @@ namespace karl_axels_cmd_adventure
                     Console.WriteLine("Du tog upp yxan använd den för att hugga ner träden");
                     harYxa = true;
                 }
-                else 
+                else
                     Console.WriteLine("Icke gilltigt eller felstavat commando!");
 
-                
+
             }
             tempel();
-    }
-            
+        }
+
         private static void tempel()
         {
             while (true)
             {
+                Console.WriteLine("du är i templet och det är mörkt");
+                Console.Write("\n");
+                input = Console.ReadLine();
+                if (input == "gå fram")
+                    ljus = false;
+                {
+                    if (input == "ta fakla")
+                    {
+                        ljus = true;
+                    }
 
+
+                    if (ljus)
+                    {
+                        Console.WriteLine("du kan gå vidare");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Det är för mörkt, försök att hitta någon fakla ");
+                    }
+
+                }
             }
         }
     }
